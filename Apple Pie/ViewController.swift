c3311ce9a8511a7ac4c9c3028ee2231f67489a37
Getu Gebre <getugebre@getus-macbook-air.local>
2019-02-19 23:11:04 -0600

    update win lose statements for players

--- Apple Pie/ViewController.swift
+++ Apple Pie/ViewController.swift
@@ -9,34 +9,37 @@
 import UIKit
 
 class ViewController: UIViewController {
-    var listOfWords = ["basketball", "noodles", "soccer", "reading", "elephant", "coding", "computer","dresser", "plates"]
+    var listOfWords = ["basketball", "handball", "soccer", "football", "hockey", "baseball", "athletics"]
     let incorrectMovesAllowed = 7
     
-    var totalWins = 0 {
+    var totalWins = [0,0] {
         didSet {
             newRound()
         }
     }
-    var totalLoses = 0 {
+    var totalLoses = [0,0] {
         didSet {
             newRound()
         }
     }
+    
     //correct guess by player 1
-    var correctGuess0 = 0
+    var correctGuess0 = 0 
     //correct guess by player 2
-    var correctGuess1 = 0 
+    var correctGuess1 = 0
     
     var correctGuessupdate = 0 {
         didSet {
             newRound()
+        }
     }
-}
     var playerSwitch : Int = 0
     @IBOutlet weak var correctWordLabel: UILabel!
     @IBOutlet weak var treeImageView: UIImageView!
     
     @IBOutlet weak var scoreLabel: UILabel!
+    
+   
     
     
     @IBOutlet var letterButtons: [UIButton]!
@@ -49,8 +52,10 @@ class ViewController: UIViewController {
         playerSwitch = currentGame.players
         if playerSwitch == 0 {
             correctGuess0 = currentGame.correctGuesses0
+            
         } else {
             correctGuess1 = currentGame.correctGuesses1
+            
         }
         
         
@@ -66,11 +71,20 @@ class ViewController: UIViewController {
     }
     func updateGameState(){
         if currentGame.incorrectMovesRemaining == 0{
-            totalLoses += 1
-            
+            //totalLoses += 1
+            if playerSwitch == 0 {
+                totalLoses[0] += 1
+            }else {
+                totalLoses[1] += 1
+            }
         }else if currentGame.word == currentGame.formattedWord {
-            totalWins += 1
-            correctGuessupdate += 5
+            //totalWins += 1
+            if playerSwitch == 0 {
+                totalWins[0] += 1
+            }else {
+                totalWins[1] += 1
+            }
+            
         } else {
             updateUI()
         }
@@ -79,26 +93,25 @@ class ViewController: UIViewController {
     func newRound (){
         if !listOfWords.isEmpty {
             let randInt = Int.random(in: 0..<listOfWords.count)
-            //let newWord = listOfWords.removeFirst()
-            let newWord = listOfWords.remove(at: randInt)
+            
+            let newWord = listOfWords[randInt]
             currentGame = Game(word: newWord, incorrectMovesRemaining: incorrectMovesAllowed, guessedLetters : [], correctGuesses0: correctGuess0,correctGuesses1: correctGuess1, players: playerSwitch)
             enableLetterButtons(true)
             updateUI()
         } else {
             enableLetterButtons(false)
         }
-       
+        
     }
     func updateUI(){
         var letters = [String]()
-//        for letter in currentGame.formattedWord {
-//            letters.append(String(letter))
-//        }
+        
         letters = currentGame.formattedWord.map({String($0)})
         
         let wordWithSpacing = letters.joined(separator: " ")
         correctWordLabel.text = wordWithSpacing
-        scoreLabel.text = "Wins: \(totalWins),  Losses: \(totalLoses),  player 1 score:\(correctGuess0),    player 2 score:\(correctGuess1),     player:\(currentGame.players)"
+        scoreLabel.text = "Wins: \(totalWins[0]),  Losses: \(totalLoses[0]),  player 1 score:\(correctGuess0), Wins: \(totalWins[1]),  Losses: \(totalLoses[1]),   player 2 score:\(correctGuess1),     player:\(currentGame.players)"
+        
         treeImageView.image = UIImage(named: "Tree \(currentGame.incorrectMovesRemaining)")
     }
     func enableLetterButtons (_ enable: Bool){
@@ -106,7 +119,6 @@ class ViewController: UIViewController {
             button.isEnabled = enable
         }
     }
-
-
+    
+    
 }
-
